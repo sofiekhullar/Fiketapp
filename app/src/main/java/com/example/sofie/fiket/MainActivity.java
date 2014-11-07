f1357d597be9287d46c2663058de6f1929237ac4
Utanforskapet <jelmesioo@gmail.com>
2014-11-07 13:49:07 +0100

    Testar att lägga till kommentar

--- app/src/main/java/com/example/sofie/fiket/MainActivity.java
+++ app/src/main/java/com/example/sofie/fiket/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Hej jag tycker detta är läskigt
+        //jag med
     }
 
 
