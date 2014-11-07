83ac5232b1d59d4dc06074d406fc2d7a7799558c
sarahyeahh <sarah_f_92@hotmail.com>
2014-11-07 14:20:23 +0100

    hej

--- app/src/main/java/com/example/sofie/fiket/MainActivity.java
+++ app/src/main/java/com/example/sofie/fiket/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         //Hej jag tycker detta är läskigt
         //jag med
+        //blä
     }
 
 
